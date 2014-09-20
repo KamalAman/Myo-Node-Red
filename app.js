5e042e7fbeab5b1b6e2953517707f07063c90a41
Kamal Aman <kaman@majiksystems.com>
2014-09-20 03:32:19 -0400

    Node-red initialized

---
+++ app.js
@@ -0,0 +1,37 @@
+var http = require('http');
+var express = require("express");
+var RED = require("node-red");
+var path = require('path');
+
+// Create an Express app
+var app = express();
+
+// Create a server
+var server = http.createServer(app);
+
+// Create the settings object
+var settings = {
+    httpAdminRoot:"/",
+    httpNodeRoot: "/api",
+    userDir:"./",
+    flowFile: path.join(__dirname, "./flows/test.json"),
+    nodesDir: path.join(__dirname, "./nodes")
+};
+
+// Initialise the runtime with a server and settings
+RED.init(server,settings);
+
+// Serve the editor UI from /red
+app.use(settings.httpAdminRoot,RED.httpAdmin);
+
+// Serve the http nodes UI from /api
+app.use(settings.httpNodeRoot,RED.httpNode);
+
+
+server.listen(1880);
+
+
+RED.start();
+
+
+
