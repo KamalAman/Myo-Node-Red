7a5b39e3eeef3a9b5de9260fc1c1693fb737d8a2
Kamal Aman <kaman@majiksystems.com>
2014-09-20 05:31:34 -0400

    Myo and Device Implementation

--- app.js
+++ app.js
@@ -1,37 +1,50 @@
-var http = require('http');
-var express = require("express");
-var RED = require("node-red");
-var path = require('path');
+/*
+ var http = require('http');
+ var express = require("express");
+ var RED = require("node-red");
+ var path = require('path');
 
-// Create an Express app
-var app = express();
+ // Create an Express app
+ var app = express();
 
-// Create a server
-var server = http.createServer(app);
+ // Create a server
+ var server = http.createServer(app);
 
-// Create the settings object
-var settings = {
-    httpAdminRoot:"/",
-    httpNodeRoot: "/api",
-    userDir:"./",
-    flowFile: path.join(__dirname, "./flows/test.json"),
-    nodesDir: path.join(__dirname, "./nodes")
-};
+ // Create the settings object
+ var settings = {
+ httpAdminRoot:"/",
+ httpNodeRoot: "/api",
+ userDir:"./",
+ flowFile: path.join(__dirname, "./flows/test.json"),
+ nodesDir: path.join(__dirname, "./nodes")
+ };
 
-// Initialise the runtime with a server and settings
-RED.init(server,settings);
+ // Initialise the runtime with a server and settings
+ RED.init(server,settings);
 
-// Serve the editor UI from /red
-app.use(settings.httpAdminRoot,RED.httpAdmin);
+ // Serve the editor UI from /red
+ app.use(settings.httpAdminRoot,RED.httpAdmin);
 
-// Serve the http nodes UI from /api
-app.use(settings.httpNodeRoot,RED.httpNode);
-
-
-server.listen(1880);
+ // Serve the http nodes UI from /api
+ app.use(settings.httpNodeRoot,RED.httpNode);
 
 
-RED.start();
+ server.listen(1880);
+
+
+ RED.start();
 
 
 
+ */
+
+var Myo = require('./lib/Myo/Myo');
+
+var x = new Myo();
+
+
+
+
+
+//setInterval(requestVibrate, 3000);
+//setInterval(requestSignal, 3000);
